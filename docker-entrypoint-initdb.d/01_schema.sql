15c30b9b9998ec347f6a9350dd99e17696afa0e2
ArtDark <artdark@icloud.com>
2021-07-07 23:33:14 +0300

    add docker-entrypoint-initdb.d

---
+++ docker-entrypoint-initdb.d/01_schema.sql
@@ -0,0 +1,45 @@
+
+CREATE TABLE clients
+(
+    id        BIGSERIAL PRIMARY KEY,
+    login     TEXT      NOT NULL UNIQUE,
+    password  TEXT      NOT NULL,
+    full_name TEXT      NOT NULL,
+    passport  TEXT      NOT NULL,
+    birthday  DATE      NOT NULL,
+    status    TEXT      NOT NULL DEFAULT 'INACTIVE' CHECK (status IN ('INACTIVE', 'ACTIVE')),
+    created   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cards
+(
+    id       BIGSERIAL PRIMARY KEY,
+    number   TEXT      NOT NULL,
+    balance  BIGINT    NOT NULL DEFAULT 0,
+    issuer   TEXT      NOT NULL CHECK ( issuer IN ('Visa', 'MasterCard', 'MIR') ),
+    holder   TEXT      NOT NULL,
+    owner_id BIGINT    NOT NULL REFERENCES clients(id),
+    status   TEXT      NOT NULL DEFAULT 'INACTIVE' CHECK (status IN ('INACTIVE', 'ACTIVE')),
+    created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name     TEXT      NOT NULL
+);
+
+CREATE TABLE icons
+(   pic      BYTEA     NOT NULL default 'some picture',
+    name     TEXT      NOT NULL PRIMARY KEY,
+    status   TEXT      NOT NULL DEFAULT 'ACTIVE' CHECK (status IN ('INACTIVE', 'ACTIVE')),
+    created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transactions
+(
+    id                 BIGSERIAL PRIMARY KEY, --ID транзакции
+    card_name          TEXT      NOT NULL,
+    bill               BIGINT    NOT NULL,
+    transaction_type   TEXT      NOT NULL,
+    category           TEXT      NOT NULL,
+    owner_id           BIGINT    NOT NULL REFERENCES cards(id),
+    icon               TEXT      NOT NULL REFERENCES icons(name),
+    status             TEXT      NOT NULL DEFAULT 'ACTIVE' CHECK (status IN ('INACTIVE', 'ACTIVE')),
+    created            TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
